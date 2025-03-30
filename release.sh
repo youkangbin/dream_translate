52c5d72dd168f5be7d04e268645e12deaa551f0a
KangBin <youkangbin@live.cn>
2025-03-30 10:55:50 +0800

    fix error via run pack shell in Linux

--- release.sh
+++ release.sh
@@ -1,33 +1,16 @@
 #!/bin/bash
 
 DIST=./dist
-DIST2=./dist_firefox
 
 # ======== Chrome & Edge ========
-# 拷贝
+# 拷贝（去掉 src 后面的斜杠，确保复制整个目录）
 echo "cp to ... $DIST"
-mkdir -p $DIST
-\cp -af ./src/ $DIST
+mkdir -p "$DIST"
+\cp -af ./src "$DIST/"  # 关键修改：去掉 src 后的斜杠
 
 # 打包 (Chrome Edge)
-sed -i "" -e "s/const isDebug = true/const isDebug = false/g" $DIST/js/common.js
-zip -rq dist.zip $DIST
-
-# ======== Firefox ========
-# 拷贝
-echo "cp to ... $DIST2"
-mkdir -p $DIST2
-\cp -af ./src/ $DIST2
-
-# 替换
-sed -i "" -e "s/const isDebug = true/const isDebug = false/g" $DIST2/js/common.js
-sed -i "" -e '16,21d' $DIST2/css/content.css # 清理 CSS 样式
-sed -i "" -e '/"tts",/d' $DIST2/manifest.json
-sed -i "" -e '/"global": true,/d' $DIST2/manifest.json
-
-# 打包 (Firefox)
-zip -rq dist_firefox.zip $DIST2
+sed -i "s/const isDebug = true/const isDebug = false/g" "$DIST/src/js/common.js"  # 路径修正
+zip -rq dist.zip "$DIST"
 
 # 清理目录
-rm -rf $DIST
-rm -rf $DIST2
+rm -rf "$DIST"
